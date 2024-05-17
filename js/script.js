e5117accc5bf610c57adf56edab506c5192f8f5c
Brian Teeters <BrianTeetersDesign@gmail.com>
2024-05-17 09:37:05 -0400

    New Book form appears on "NEW BOOK" button click

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
-const library = document.querySelector('#library')
+const library = document.querySelector('#library');
+const newBookBtn = document.querySelector('.newBookBtn');
+const bookForm = document.querySelector('#hideForm');
+
 const myLibrary = [];
 
+// Book Object Constructor
 function Book(title, author, pageCount, read){
     this.title = title;
     this.author = author;
@@ -11,17 +15,17 @@ function Book(title, author, pageCount, read){
     };
 }
 
+// creating and adding books to "myLibrary" array
 const book1 = new Book('First Book', 'Some dude', 100, 'Not read yet');
 const book2 = new Book('Moby Dick', 'Mark Twain', 423, 'Have read');
 myLibrary.push(book1);
 myLibrary.push(book2);
 
-
+// Add book to library list
 function addBookToLibrary(title, author, pageCount, read) {
-
-    const newBook = new Book()
 }
 
+// Display books in "library" div
 function showBooks(){
     for (let i = 0; i < myLibrary.length; i++) {
             const newBook = document.createElement("div");
@@ -33,10 +37,15 @@ function showBooks(){
             newBook.setAttribute("class", "book");
             
             library.appendChild(newBook);
-            console.log(myLibrary[i]);
+            console.log(myLibrary[i].info());
         }    
 }
 
+// Used to display books in library on page load
 window.onload = function(){
     showBooks();
 }
+
+newBookBtn.addEventListener("click", () => {
+    bookForm.setAttribute("id", "showForm");
+})
